53c734a8b643b7099a884b5776e9636103379f83
freeefly <freefly3557@gmail.com>
2021-04-17 23:21:29 +0900

    feat : set App component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 ﻿<template>
-  <div></div>
+  <div>
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
 </template>
 
 <script>
+import TodoHeader from "@/components/TodoHeader";
+import TodoInput from "@/components/TodoInput";
+import TodoList from "@/components/TodoList";
+import TodoFooter from "@/components/TodoFooter";
 export default {
+  components: {TodoFooter, TodoList, TodoInput, TodoHeader},
   data() {
     return {
 
@@ -15,4 +25,16 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style>
+  body {
+    text-align: center;
+    background-color: #F6F6F6;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
+</style>
